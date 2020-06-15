a67252a3b6352d7f34cca2720f416112e8c31cbe
takeshi shimada <konohazukux@gmail.com>
2020-06-15 15:56:01 +0900

    fix

---
+++ SampleAppRealm/Sample1128/Onigiri.swift
@@ -0,0 +1,14 @@
+//
+//  Onigiri.swift
+//  Sample1128
+//
+//  Created by koharu shimada on 2020/06/15.
+//  Copyright © 2020 TAKESHI SHIMADA. All rights reserved.
+//
+
+import Foundation
+
+class Onigiri {
+    
+}
+
