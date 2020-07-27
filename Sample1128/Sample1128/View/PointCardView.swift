77e8f60deea64ce856fb4746d09879912bc7a119
takeshi shimada <konohazukux@gmail.com>
2020-07-27 18:41:16 +0900

    fix

---
+++ Sample1128/Sample1128/View/PointCardView.swift
@@ -0,0 +1,33 @@
+//
+//  PointHistoryView.swift
+//  Sample1128
+//
+//  Created by t shimada on 2020/07/27.
+//  Copyright © 2020 TAKESHI SHIMADA. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PointCardView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadNib()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        loadNib()
+    }
+
+    func loadNib() {
+        if let view = Bundle(for: type(of: self)).loadNibNamed(String(describing: type(of: self)), owner: self, options: nil)?.first as? UIView {
+            self.addSubview(view)
+            view.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+            view.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+            view.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+            view.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        }
+    }
+}
