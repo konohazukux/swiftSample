a3402bf28bd9b20e35c998af0dcc5b842c2ee2b7
takeshi shimada <konohazukux@gmail.com>
2020-07-28 19:25:23 +0900

    fix

--- Sample1128/Sample1128/View/PointSelectPeriodView.swift
+++ Sample1128/Sample1128/View/PointSelectPeriodView.swift
@@ -11,6 +11,9 @@ import UIKit
 
 class PointSelectPeriodView: UIView, NibLoaderProtocol  {
 
+    @IBOutlet private weak var periodView: UIView!
+    @IBOutlet private weak var doneButton: UIView!
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         _loadNib()
@@ -23,6 +26,13 @@ class PointSelectPeriodView: UIView, NibLoaderProtocol  {
 
     func _loadNib() {
         addNibWithLayout()
+        setupUI()
+    }
+
+    func setupUI() {
+        periodView.corner(radius: 8.0)
+        periodView.border(width: 1.0, color: .lightGray)
+        doneButton.corner(radius: 6.0)
     }
 
 }
