1807b6fb1efcbc09b98b475249096b64e21c6602
takeshi shimada <konohazukux@gmail.com>
2020-08-14 10:25:12 +0900

    fix#

---
+++ Sample1128/Sample1128/UsageLimited/UsageLimitedVC.swift
@@ -0,0 +1,18 @@
+//
+//  UsageLimitedVC.swift
+//  ToyotaWallet
+//
+//  Created by t shimada on 2020/07/29.
+//
+
+import RxCocoa
+import RxSwift
+import UIKit
+
+final class UsageLimitedVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
