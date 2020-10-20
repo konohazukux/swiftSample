ec39784a19295b3db706607f5928f545e19931f5
takeshi shimada <konohazukux@gmail.com>
2020-10-20 16:54:43 +0900

    fix

--- Sample1128/Sample1128/FirstViewController.swift
+++ Sample1128/Sample1128/FirstViewController.swift
@@ -12,6 +12,10 @@ import RxSwift
 import RxCocoa
 import SwiftDate
 
+struct User: Codable {
+    var name: String
+}
+
 class FirstViewController: UIViewController {
 
     @IBOutlet weak var label: UILabel!
@@ -22,26 +26,81 @@ class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        show()
+        
+        UITabBar.appearance().layer.borderWidth = 0.5
+        UITabBar.appearance().layer.borderColor = UIColor.clear.cgColor
+        UITabBar.appearance().clipsToBounds = true
+
+
+        UITabBar.appearance().shadowImage = UIImage.colorForNavBar(color: UIColor.white)
+        UITabBar.appearance().backgroundImage = UIImage.colorForNavBar(color: UIColor.white)
+
+
+        let user1 = User.init(name: "taro")
+        let user2 = User.init(name: "taro2")
+        let users = [ user1, user2]
+
+        let someKey = Key<[User]>("someKey")
+        Defaults.shared.set(users, for: someKey)
+
+        
+        if let temp = Defaults.shared.get(for: someKey) {
+            print(temp)
+            print(temp)
+
+        }
+    
+        inputPaymentcodeAlert()
+        
     }
     
-    func show() {
-       
-        let storyboard = UIStoryboard.init(name: "AmountLimit", bundle: nil)
-        guard let viewController = storyboard.instantiateInitialViewController() as? AmountLimitVC else { return }
-        
-//        let storyboard = UIStoryboard.init(name: "AmountLimitCardInfo", bundle: nil)
-//        guard let viewController = storyboard.instantiateInitialViewController() as? AmountLimitCardInfoVC else { return }
-       
-        
-        present(viewController, animated: true, completion: nil)
-        
+    private func inputPaymentcodeAlert() {
+        let alert = UIAlertController(title: "お支払いコード", message: "6桁のお支払いコードを入力してください。", preferredStyle: .alert)
+        if #available(iOS 13, *) {
+            alert.overrideUserInterfaceStyle = .light
+        }
+        alert.view.tintColor = UIColor.red
+        // textfiledの追加
+        alert.addTextField { [weak self] textField in
+            // PlaceFolderなどあればここで実装
+            textField.placeholder = "6桁のお支払いコード"
+            textField.keyboardType = .numberPad
+//            textField.delegate = self
+        }
 
+        // ①OK ボタン
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: { _ in
+            // 入力された番号を取得
+            let textField = alert.textFields?.first
+//            self.createOrder(order: textField?.text ?? "", cardReference: nil)
+        })
+        alert.addAction(okAction)
+        alert.preferredAction = okAction
+        // ➁キャンセル　ボタン
+        // ・キャンセルボタンを押すと、QR決済読取画面に戻る
+        let cancelAction = UIAlertAction(title: "キャンセル", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+
+        present(alert, animated: true, completion: nil)
     }
     
 
 }
 
+extension UIImage {
+    class func colorForNavBar(color: UIColor) -> UIImage {
+        let rect = CGRect.init(x: 0.0, y: 0.0, width: 1.0, height: 1.0)
+        UIGraphicsBeginImageContext(rect.size)
+        if let context = UIGraphicsGetCurrentContext() {
+            context.setFillColor(color.cgColor)
+            context.fill(rect)
+            guard let image = UIGraphicsGetImageFromCurrentImageContext() else { return UIImage() }
+            UIGraphicsEndImageContext()
+            return image
+        }
+        return UIImage()
+    }
+}
 
 
 
