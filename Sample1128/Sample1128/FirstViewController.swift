3d27bd9c775f275d550ad00ec8cb4174f48aab43
takeshi shimada <konohazukux@gmail.com>
2021-07-24 14:22:30 +0900

    fix

--- Sample1128/Sample1128/FirstViewController.swift
+++ Sample1128/Sample1128/FirstViewController.swift
@@ -30,76 +30,17 @@ class FirstViewController: UIViewController {
     let disposeBag = DisposeBag()
     let testRelay = PublishRelay<Int>()
 
-    let viewModel = FirstViewModel()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        UITabBar.appearance().layer.borderWidth = 0.5
-        UITabBar.appearance().layer.borderColor = UIColor.clear.cgColor
-        UITabBar.appearance().clipsToBounds = true
-
 
     }
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-       
-        temp()
-       
-//        viewModel.eventGeusture(state: .began, positionY: 140)
-//        viewModel.eventGeusture(state: .changed, positionY: 135)
-//        viewModel.eventGeusture(state: .changed, positionY: 130)
-//        viewModel.eventGeusture(state: .changed, positionY: 125)
-//        viewModel.eventGeusture(state: .changed, positionY: 120)
-//        viewModel.eventGeusture(state: .changed, positionY: 110)
-//        viewModel.eventGeusture(state: .changed, positionY: 112)
-//        viewModel.eventGeusture(state: .changed, positionY: 115)
-//        viewModel.eventGeusture(state: .ended, positionY: 118)
-//
-//
-//        viewModel.eventGeusture(state: .began, positionY: 40)
-//        viewModel.eventGeusture(state: .ended, positionY: 35)
-        
-        
-    }
-    
-    func temp() {
-        
-        viewModel.pergentageRelay
-            .asDriver()
-            .drive(onNext: { [unowned self] val in
-                let height = (100.0 - val)
-                self.heightConstraint.constant = height
-            })
-            .disposed(by: disposeBag)
-        
-        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: nil)
-        panGestureRecognizer.rx.event
-            .subscribe(onNext: { [weak self] sender in
-                guard let self = self else { return }
-                let currentPointY = sender.location(in: self.view).y
-                switch sender.state {
-                case .began:
-                    self.viewModel.eventGeusture(state: .began, positionY: currentPointY)
-                case .changed:
-                    self.viewModel.eventGeusture(state: .changed, positionY: currentPointY)
-                case .ended:
-                    self.viewModel.eventGeusture(state: .ended, positionY: currentPointY)
-                default:
-                    break
-                }
-            })
-            .disposed(by: disposeBag)
-        view.addGestureRecognizer(panGestureRecognizer)
-
-    }
-    
-    private func showSetting() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) { [weak self] in
-            let sb = UIStoryboard.init(name: "SettingViewController", bundle: nil)
-            if let vc = sb.instantiateInitialViewController() as? SettingViewController {
-                self?.navigationController?.pushViewController(vc, animated: true)
-            }
-        }
-    }
-    
 }
+
+
+class Sample1ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
