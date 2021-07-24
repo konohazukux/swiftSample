8c8f92cc5595655b0350ada404fb85f8d01a768e
takeshi shimada <konohazukux@gmail.com>
2021-07-24 15:01:49 +0900

    fix

--- Sample1128/Sample1128/FirstViewController.swift
+++ Sample1128/Sample1128/FirstViewController.swift
@@ -33,14 +33,44 @@ class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+      
+        let vc = Sample1ViewController()
+        let navi = UINavigationController.init(rootViewController: vc)
+        navi.modalPresentationStyle = .fullScreen
+        present(navi, animated: true, completion: nil)
+        
+        
     }
 }
 
-
 class Sample1ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor.init(red: 0, green: 1, blue: 1, alpha: 1)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+            print("111 sdfinfolog-\(#line) \(type(of: self))  \(#function) : \(self) ")
+            let vc = Sample2ViewController()
+            self?.navigationController?.pushViewController(vc, animated: true)
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) { [weak self] in
+            print("111 sdfinfolog-\(#line) \(type(of: self))  \(#function) : \(self) ")
+            self?.dismiss(animated: true, completion: nil)
+        }
     }
+}
+
+class Sample2ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.init(red: 0, green: 0, blue: 1, alpha: 1)
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        let isMovingFromParent = self.isMovingFromParent
+        print("111 sdfinfolog-\(#line) \(type(of: self))  \(#function) : \(isMovingFromParent) ")
+    }
+    
 }
 
