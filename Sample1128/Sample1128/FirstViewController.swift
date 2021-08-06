8840e97a66df9a37d22b60b2374355736d76197d
takeshi shimada <konohazukux@gmail.com>
2021-08-06 16:26:25 +0900

    fix

--- Sample1128/Sample1128/FirstViewController.swift
+++ Sample1128/Sample1128/FirstViewController.swift
@@ -7,16 +7,6 @@
 //
 
 import UIKit
-import SafariServices
-import RxSwift
-import RxCocoa
-import SwiftDate
-
-struct User: Codable {
-    var name: String
-}
-
-var testCount = 0
 
 class FirstViewController: UIViewController {
 
@@ -25,21 +15,13 @@ class FirstViewController: UIViewController {
     @IBOutlet weak var oneButton: UIButton!
     @IBOutlet weak var label1: UILabel!
     @IBOutlet weak var heightConstraint: NSLayoutConstraint!
-    
-    private var myButton: UIButton!
-    let disposeBag = DisposeBag()
-    let testRelay = PublishRelay<Int>()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-      
         let vc = Sample1ViewController()
         let navi = UINavigationController.init(rootViewController: vc)
         navi.modalPresentationStyle = .fullScreen
         present(navi, animated: true, completion: nil)
-        
-        
     }
 }
 
@@ -49,28 +31,64 @@ class Sample1ViewController: UIViewController {
         self.view.backgroundColor = UIColor.init(red: 0, green: 1, blue: 1, alpha: 1)
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
-            print("111 sdfinfolog-\(#line) \(type(of: self))  \(#function) : \(self) ")
             let vc = Sample2ViewController()
             self?.navigationController?.pushViewController(vc, animated: true)
         }
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2) { [weak self] in
-            print("111 sdfinfolog-\(#line) \(type(of: self))  \(#function) : \(self) ")
-            self?.dismiss(animated: true, completion: nil)
-        }
     }
 }
 
 class Sample2ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor.init(red: 0, green: 1, blue: 0, alpha: 1)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+            let vc = Sample3ViewController()
+            self?.navigationController?.pushViewController(vc, animated: true)
+        }
+
+    }
+}
+
+class Sample3ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
         self.view.backgroundColor = UIColor.init(red: 0, green: 0, blue: 1, alpha: 1)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+            let vc = Sample4ViewController()
+            self?.navigationController?.pushViewController(vc, animated: true)
+        }
+
+    }
+}
+
+class Sample4ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.init(red: 1, green: 1, blue: 0, alpha: 1)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+
+            // a. popViewController
+            //self?.navigationController?.popViewController(animated: true)
+
+            // b. popToRootViewController
+            if let vc2 = (self?.navigationController?.viewControllers.filter { $0.isKind(of: Sample3ViewController.self)})?.first {
+                print("111 sdfinfolog-\(#line) \(type(of: self))  \(#function) : \(vc2) ")
+                self?.navigationController?.popToRootViewController(animated: true)
+            }
+           
+            // c. popToViewController
+            //self?.navigationController?.popToViewController(vc2, animated: true)
+        }
     }
     override func viewWillDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         let isMovingFromParent = self.isMovingFromParent
+        let isBeingDismissed = self.isBeingDismissed
         print("111 sdfinfolog-\(#line) \(type(of: self))  \(#function) : \(isMovingFromParent) ")
+        print("111 sdfinfolog-\(#line) \(type(of: self))  \(#function) : \(isBeingDismissed) ")
     }
-    
 }
-
