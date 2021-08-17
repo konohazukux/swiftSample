8ded35e4f099939bf7032f51017675c334105119
takeshi shimada <konohazukux@gmail.com>
2021-08-17 19:59:07 +0900

    clear

--- Sample1128/Sample1128/FirstViewController.swift
+++ Sample1128/Sample1128/FirstViewController.swift
@@ -9,9 +9,8 @@
 import UIKit
 import RxSwift
 import RxCocoa
-import CoreLocation
 
-class FirstViewController: BaseViewController {
+class FirstViewController: UIViewController {
 
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var phoneTf: UITextField!
@@ -19,57 +18,15 @@ class FirstViewController: BaseViewController {
     @IBOutlet weak var label1: UILabel!
     @IBOutlet weak var heightConstraint: NSLayoutConstraint!
 
-    var locationManager: CLLocationManager!
-
-    
-    private var myButton: UIButton!
     let disposeBag = DisposeBag()
-    let testRelay = PublishRelay<Int>()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
     
-   
-        setupLocationManager()
+    func temp() {
         
     }
-   
-    func setupLocationManager() {
-        locationManager = CLLocationManager() // インスタンスの生成
-        locationManager.delegate = self // CLLocationManagerDelegateプロトコルを実装するクラスを指定する
-        
-//        locationManager.requestWhenInUseAuthorization() //
-//        locationManager.requestAlwaysAuthorization()
-
-    }
-
-}
-
-extension FirstViewController: CLLocationManagerDelegate {
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        switch status {
-        case .notDetermined:
-            print("ユーザーはこのアプリケーションに関してまだ選択を行っていません")
-            locationManager.requestWhenInUseAuthorization() //
-//            locationManager.requestAlwaysAuthorization()
-            // 許可を求めるコードを記述する（後述）
-            break
-        case .denied:
-            print("ローケーションサービスの設定が「無効」になっています (ユーザーによって、明示的に拒否されています）")
-            // 「設定 > プライバシー > 位置情報サービス で、位置情報サービスの利用を許可して下さい」を表示する
-            break
-        case .restricted:
-            print("このアプリケーションは位置情報サービスを使用できません(ユーザによって拒否されたわけではありません)")
-            // 「このアプリは、位置情報を取得できないために、正常に動作できません」を表示する
-            break
-        case .authorizedAlways:
-            print("常時、位置情報の取得が許可されています。")
-            // 位置情報取得の開始処理
-            break
-        case .authorizedWhenInUse:
-            print("起動時のみ、位置情報の取得が許可されています。")
-            // 位置情報取得の開始処理
-            break
-        }
-    }
+    
+    
 }
