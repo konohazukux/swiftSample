6e5d4cced409c5fb8b254b16be2e12d14d520e37
takeshi shimada <konohazukux@gmail.com>
2021-08-21 10:19:56 +0900

    clear

--- Sample1128/Sample1128/FirstViewController.swift
+++ Sample1128/Sample1128/FirstViewController.swift
@@ -12,51 +12,9 @@ import RxCocoa
 
 class FirstViewController: UIViewController {
 
-    @IBOutlet weak var label: UILabel!
-    @IBOutlet weak var phoneTf: UITextField!
-    @IBOutlet weak var oneButton: UIButton!
-    @IBOutlet weak var label1: UILabel!
-    @IBOutlet weak var heightConstraint: NSLayoutConstraint!
-
-    @IBOutlet weak var pageBaseView: UIView!
-
-
-    var pageViewController: UIPageViewController!
-    
     let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        pageview()
-    }
-    
-    func pageview() {
-     
-        let label = UILabel()
-        label.frame.size = CGSize(width: 200, height: 100)
-        label.center = view.center
-        label.text = "ベースのview"
-        label.textAlignment = .center
-        view.addSubview(label)
-
-        // コンテナに追加
-        let pageVC = PageViewController(transitionStyle: .scroll,
-                                        navigationOrientation: .horizontal,
-                                        options: nil)
-        let pageViewHeight: CGFloat = 300
-//        pageVC.view.frame = CGRect(x: 0, y: view.bounds.size.height - pageViewHeight, width: view.bounds.size.width, height: pageViewHeight)
-        pageVC.view.frame = .zero
-
-        addChild(pageVC)
-        pageBaseView.addSubview(pageVC.view)
-        pageVC.view.translatesAutoresizingMaskIntoConstraints = false
-        pageBaseView.topAnchor.constraint(equalTo: pageVC.view.topAnchor).isActive = true
-        pageBaseView.bottomAnchor.constraint(equalTo: pageVC.view.bottomAnchor).isActive = true
-        pageBaseView.leadingAnchor.constraint(equalTo: pageVC.view.leadingAnchor).isActive = true
-        pageBaseView.trailingAnchor.constraint(equalTo: pageVC.view.trailingAnchor).isActive = true
-        
-//        view.addSubview(pageVC.view)
-        pageVC.didMove(toParent: self)
-
     }
 }
