de0d7d3594e076770b8fb90a366653f614184773
takeshi shimada <konohazukux@gmail.com>
2021-09-15 13:22:45 +0900

    fix

--- Sample1128/Sample1128/FirstViewController.swift
+++ Sample1128/Sample1128/FirstViewController.swift
@@ -15,7 +15,32 @@ class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+        let line = DotLineView(frame: .zero)
+        line.frame = CGRect.init(x: 100, y: 400, width: 100, height: 1)
+        view.addSubview(line)
+        
+        
     }
 
+    
+    
 }
 
+
+class DotLineView: UIView {
+   
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initialize()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+   
+    private func initialize() {
+        self.backgroundColor = UIColor.init(red: 0, green: 1, blue: 0, alpha: 1)
+        
+    }
+    
+}
