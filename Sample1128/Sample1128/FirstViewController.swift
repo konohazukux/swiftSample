b3f5ae4cae157bfca3cd1c6171a50f5cd72b2219
takeshi shimada <konohazukux@gmail.com>
2021-09-16 14:28:33 +0900

    reset

--- Sample1128/Sample1128/FirstViewController.swift
+++ Sample1128/Sample1128/FirstViewController.swift
@@ -18,26 +18,5 @@ class FirstViewController: UIViewController {
        
     }
 
-    
 }
 
-
-@IBDesignable
-class DotLineView: UIView {
-    // Inspectorで点線の色と幅を設定出来るようにする
-    @IBInspectable var dotColor: UIColor = UIColor.gray
-    @IBInspectable var lineWidth: CGFloat = 2.0
-    @IBInspectable var dashWidth: CGFloat = 2.0
-
-    override func draw(_ rect: CGRect) {
-        let path = UIBezierPath()
-        path.lineWidth = self.lineWidth
-        path.lineCapStyle = .butt
-        path.move(to: CGPoint(x: 0, y: rect.midY))
-        path.addLine(to: CGPoint(x: rect.width, y: rect.midY))
-        let dashes = [dashWidth, dashWidth]
-        path.setLineDash(dashes, count: dashes.count, phase: 0)
-        dotColor.setStroke()
-        path.stroke()
-    }
-}
