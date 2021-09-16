4ff1c746eeeab4ad0319dab7790535feb75e29b3
takeshi shimada <konohazukux@gmail.com>
2021-09-16 14:27:46 +0900

    fix

--- Sample1128/Sample1128/FirstViewController.swift
+++ Sample1128/Sample1128/FirstViewController.swift
@@ -16,31 +16,28 @@ class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
        
-        let line = DotLineView(frame: .zero)
-        line.frame = CGRect.init(x: 100, y: 400, width: 100, height: 1)
-        view.addSubview(line)
-        
-        
     }
 
-    
     
 }
 
 
+@IBDesignable
 class DotLineView: UIView {
-   
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        initialize()
+    // Inspectorで点線の色と幅を設定出来るようにする
+    @IBInspectable var dotColor: UIColor = UIColor.gray
+    @IBInspectable var lineWidth: CGFloat = 2.0
+    @IBInspectable var dashWidth: CGFloat = 2.0
+
+    override func draw(_ rect: CGRect) {
+        let path = UIBezierPath()
+        path.lineWidth = self.lineWidth
+        path.lineCapStyle = .butt
+        path.move(to: CGPoint(x: 0, y: rect.midY))
+        path.addLine(to: CGPoint(x: rect.width, y: rect.midY))
+        let dashes = [dashWidth, dashWidth]
+        path.setLineDash(dashes, count: dashes.count, phase: 0)
+        dotColor.setStroke()
+        path.stroke()
     }
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-   
-    private func initialize() {
-        self.backgroundColor = UIColor.init(red: 0, green: 1, blue: 0, alpha: 1)
-        
-    }
-    
 }
