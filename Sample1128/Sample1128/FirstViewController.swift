6566899e3d146ae2e596e98da1fc81522b8df8c4
takeshi shimada <konohazukux@gmail.com>
2021-10-01 19:23:11 +0900

    fix

--- Sample1128/Sample1128/FirstViewController.swift
+++ Sample1128/Sample1128/FirstViewController.swift
@@ -11,15 +11,9 @@ import RxRelay
 class FirstViewController: UIViewController {
 
     let disposeBag = DisposeBag()
-    lazy var indicator = HUDService().bindHUD(disposeBag: disposeBag)
-    let button = UIButton()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        button.rx.tap
-            .trackActivity(indicator)
-            .subscribe()
-            .disposed(by: disposeBag)
 
     }
 }
