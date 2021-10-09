ebbd68a922d96433b45ea531c19d9dd40e0278f6
takeshi shimada <konohazukux@gmail.com>
2021-10-09 17:17:57 +0900

    fix

--- Sample1128/Sample1128/FirstViewController.swift
+++ Sample1128/Sample1128/FirstViewController.swift
@@ -4,16 +4,49 @@
 //
 
 import UIKit
-import PKHUD
 import RxSwift
-import RxRelay
+import RxCocoa
+import PAYJP
 
 class FirstViewController: UIViewController {
 
     let disposeBag = DisposeBag()
+    @IBOutlet var button: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        PAYJPSDK.publicKey = "pk_test_0383a1b8f91e8a6e3ea0e2a9"
+        PAYJPSDK.locale = Locale.current
+        
+        button.rx.tap
+            .asDriver()
+            .drive(onNext: { _ in
+                self.temp()
+            })
+            .disposed(by: disposeBag)
+            
+        
+    }
+    
+    func temp() {
+        // ViewControllerから呼び出す
+        let style = FormStyle(
+            labelTextColor: .black,
+            inputTextColor: .black,
+            tintColor: .blue)
+        let cardFormViewController = CardFormViewController.createCardFormViewController(style: style, delegate: self, viewType: .labelStyled)
+        self.navigationController?.pushViewController(cardFormViewController, animated: true)
+    }
+    
+}
+
+extension FirstViewController: CardFormViewControllerDelegate {
+    func cardFormViewController(_: CardFormViewController, didCompleteWith result: CardFormResult) {
+        print("111 sdf\(#line) \(type(of: self))  \(#function) : \(self) ")
+    }
+    
+    func cardFormViewController(_: CardFormViewController, didProduced token: Token, completionHandler: @escaping (Error?) -> Void) {
+        print("111 sdf\(#line) \(type(of: self))  \(#function) : \(self) ")
     }
 }
