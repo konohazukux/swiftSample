6d047925f62a07875ea18493e2f89a2315028b4e
takeshi shimada <konohazukux@gmail.com>
2022-01-14 11:28:19 +0900

    fix

--- SampleFM/SampleFM/Sally.swift
+++ SampleFM/SampleFM/Sally.swift
@@ -6,11 +6,20 @@
 //
 
 import Foundation
+import RxCocoa
+import RxSwift
 
 // Sally.swift
 
 public class Sally: NSObject {
+    let disposeBag = DisposeBag()
     public func sallyMethod() {
         print("Sally().sallyMethod is called !!")
+
+        Observable<Int>.just(1)
+            .subscribe(onNext: { 
+               print($0)
+            } )
+            .disposed(by: disposeBag)
     }
 }
