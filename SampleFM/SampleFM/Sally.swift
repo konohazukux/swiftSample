937f2b609376918ea73bd4e9508242ad7c3ad2a2
takeshi shimada <konohazukux@gmail.com>
2022-01-14 10:58:00 +0900

    fix

---
+++ SampleFM/SampleFM/Sally.swift
@@ -0,0 +1,16 @@
+//
+//  Sally.swift
+//  SampleFM
+//
+//  Created by TAKESHI SHIMADA on 2022/01/13.
+//
+
+import Foundation
+
+// Sally.swift
+
+public class Sally: NSObject {
+    public func sallyMethod() {
+        print("Sally().sallyMethod is called !!")
+    }
+}
