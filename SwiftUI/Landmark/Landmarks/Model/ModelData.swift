b2aa8468ef3a55c7f5562b380eab6a6ea3fe4d61
takeshi shimada <konohazukux@gmail.com>
2023-03-17 11:17:46 +0900

    fix

--- SwiftUI/Landmark/Landmarks/Model/ModelData.swift
+++ SwiftUI/Landmark/Landmarks/Model/ModelData.swift
@@ -33,3 +33,4 @@ func load<T: Decodable>(_ filename: String) -> T {
         fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
     }
 }
+
--- SwiftUI/Landmark/Landmarks/Views/Landmarks/LandmarkList.swift
+++ SwiftUI/Landmark/Landmarks/Views/Landmarks/LandmarkList.swift
@@ -10,19 +10,20 @@ import SwiftUI
 struct LandmarkList: View {
     @EnvironmentObject var modelData: ModelData
     @State private var showFavoritesOnly = false
-   
+
     var filteredLandmarks: [Landmark] {
         modelData.landmarks.filter { landmark in
             (!showFavoritesOnly || landmark.isFavorite)
         }
     }
-    
+
     var body: some View {
         NavigationView {
             List {
-                Toggle(isOn: $showFavoritesOnly){
-                  Text("Favorites only")
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
                 }
+
                 ForEach(filteredLandmarks) { landmark in
                     NavigationLink {
                         LandmarkDetail(landmark: landmark)
@@ -38,9 +39,7 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
-            LandmarkList()
-                .environmentObject(ModelData())
-        }
+        LandmarkList()
+            .environmentObject(ModelData())
     }
 }
