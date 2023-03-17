5cf69c8ec05761fb7719dfae287be31552d70ddc
takeshi shimada <konohazukux@gmail.com>
2023-03-17 10:13:25 +0900

    fix

---
+++ SwiftUI/Landmark/Landmark/Landmark.swift
@@ -0,0 +1,30 @@
+//
+//  Landmark.swift
+//  Landmark
+//
+//  Created by TAKESHI SHIMADA on 2023/03/16.
+//
+
+import Foundation
+import SwiftUI
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+    
+}
