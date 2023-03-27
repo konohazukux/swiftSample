ca645b65dd09b30bf1f7fe84e34dc861e09c7614
takeshi shimada <konohazukux@gmail.com>
2023-03-27 12:51:22 +0900

    SwiftUI  UIHostingController

---
+++ SwiftUI_UIHostingController/Sample1128/ThirdView.swift
@@ -0,0 +1,23 @@
+//
+//  ThirdView.swift
+//  Sample1128
+//
+//  Created by TAKESHI SHIMADA on 2023/03/27.
+//  Copyright © 2023 TAKESHI SHIMADA. All rights reserved.
+//
+
+import SwiftUI
+
+struct ThirdView: View {
+    
+    var body: some View {
+        Text("hello world")
+    }
+    
+}
+
+struct ThirdView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThirdView()
+    }
+}
