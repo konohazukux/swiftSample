5a44c6e5f0105b087c2a32c78bcb228e4ede1204
takeshi shimada <konohazukux@gmail.com>
2023-04-02 15:24:39 +0900

    carousel above iOS14

--- SwiftUI_List/Sample1128/ThirdView.swift
+++ SwiftUI_List/Sample1128/ThirdView.swift
@@ -10,60 +10,33 @@ import SwiftUI
 
 // https://note.com/smtakahashi/n/n819591e5905c
 struct ThirdView: View {
-    @State private var currentIndex = 0
-    @State var examples = ["1", "2", "3"]
-    @GestureState private var dragOffset: CGFloat = 0
-    
-    let itemPadding: CGFloat = 20
+    @State private var index = 0
     
     var body: some View {
-        GeometryReader { bodyView in
-            HStack(spacing: itemPadding){
-                ForEach(examples.indices, id: \.self) { index in
-                        Text(examples[index])
-                              .foregroundColor(Color.white)
-                              .font(.system(size: 50, weight: .bold))
-                              .frame(width: bodyView.size.width * 0.8, height: 300)
-                              .background(Color.gray)
-                              .padding(.leading, index == 0 ? bodyView.size.width * 0.1 : 0)
-                    }
+        VStack{
+            TabView(selection: $index) {
+                ForEach((0..<3), id: \.self) { index in
+                    CardView()
+                }
             }
-            // ドラッグした分だけoffsetを移動する
-            .offset(x: self.dragOffset)
-            // currentIndexに応じたoffsetへ移動する
-            .offset(x: -CGFloat(self.currentIndex) * (bodyView.size.width * 0.8 + itemPadding))
-            .gesture(
-                      DragGesture()
-                          .updating(self.$dragOffset, body: { (value, state, _) in
-                              // 移動幅（width）のみ更新する
-                              state = value.translation.width
-                          })
-                          .onEnded({ value in
-                              var newIndex = self.currentIndex
-                              // ドラッグ幅からページングを判定
-                              // 今回は画面幅x0.3としているが、操作感に応じてカスタマイズする必要がある
-                              if abs(value.translation.width) > bodyView.size.width * 0.3 {
-                                  newIndex = value.translation.width > 0 ? self.currentIndex - 1 : self.currentIndex + 1
-                              }
-                              
-                              // 最小ページ、最大ページを超えないようチェック
-                              if newIndex < 0 {
-                                  newIndex = 0
-                              } else if newIndex > (self.examples.count - 1) {
-                                  newIndex = self.examples.count - 1
-                              }
-                              
-                              self.currentIndex = newIndex
-                          })
-                  )
-                
-        
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
         }
+        .frame(height: 200)
     }
 }
 
 struct ThirdView_Previews: PreviewProvider {
     static var previews: some View {
         ThirdView()
+    }
+}
+
+struct CardView: View{
+    var body: some View{
+        Rectangle()
+            .fill(Color.pink)
+            .frame(height: 200)
+            .border(Color.black)
+            .padding()
     }
 }
