e76a694fbf32cbabdb6d599ca4285ad044f5234c
takeshi shimada <konohazukux@gmail.com>
2023-07-14 17:26:15 +0900

    fix

--- Apollo/Sample1128/FirstViewController.swift
+++ Apollo/Sample1128/FirstViewController.swift
@@ -16,17 +16,6 @@ class FirstViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
-            guard let self = self else { return }
-          
-            let vc = HomeBackgroundPageViewController()
-            vc.modalPresentationStyle = .overFullScreen
-            self.present(vc, animated: true)
-            
-            
-        }
-        
     }
    
 }
