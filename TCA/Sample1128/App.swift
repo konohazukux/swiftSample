0ff342982d0818b7d4d92d0a02834e9df59b775a
takeshi shimada <konohazukux@gmail.com>
2023-07-27 23:06:04 +0900

    from appDelegate  to app.swift

---
+++ TCA/Sample1128/App.swift
@@ -0,0 +1,18 @@
+//
+//  App.swift
+//  Sample1128
+//
+//  Created by TAKESHI SHIMADA on 2023/07/27.
+//  Copyright © 2023 TAKESHI SHIMADA. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct MyApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
