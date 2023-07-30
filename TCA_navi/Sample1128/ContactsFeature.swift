f3a0ca6bd3b3df0692447c5b0d5c7eba9b4bd81a
takeshi shimada <konohazukux@gmail.com>
2023-07-30 17:38:06 +0900

    fix

---
+++ TCA_navi/Sample1128/ContactsFeature.swift
@@ -0,0 +1,21 @@
+//
+//  ContactsFeature.swift
+//  Sample1128
+//
+//  Created by TAKESHI SHIMADA on 2023/07/30.
+//  Copyright © 2023 TAKESHI SHIMADA. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContactsFeature: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContactsFeature_Previews: PreviewProvider {
+    static var previews: some View {
+        ContactsFeature()
+    }
+}
