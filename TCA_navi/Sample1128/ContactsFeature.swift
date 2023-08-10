d3666a261e7de7aee234cfdca13d10d99c0b46ab
takeshi shimada <konohazukux@gmail.com>
2023-08-10 10:55:05 +0900

    step3

--- TCA_navi/Sample1128/ContactsFeature.swift
+++ TCA_navi/Sample1128/ContactsFeature.swift
@@ -56,3 +56,21 @@ struct ContentView: View {
     }
   }
 }
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView(
+      store: Store(
+        initialState: ContactsFeature.State(
+          contacts: [
+            Contact(id: UUID(), name: "Blob"),
+            Contact(id: UUID(), name: "Blob Jr"),
+            Contact(id: UUID(), name: "Blob Sr"),
+          ]
+        )
+      ) {
+        ContactsFeature()
+      }
+    )
+  }
+}
