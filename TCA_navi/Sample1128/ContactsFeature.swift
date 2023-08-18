5568d0d00ce4d50e57bd02865c4614a4454a9ea8
takeshi shimada <konohazukux@gmail.com>
2023-08-18 23:41:51 +0900

    step 8

--- TCA_navi/Sample1128/ContactsFeature.swift
+++ TCA_navi/Sample1128/ContactsFeature.swift
@@ -27,6 +27,12 @@ struct ContactsFeature: Reducer {
       case .addContact(.presented(.cancelButtonTapped)):
         state.addContact = nil
         return .none
+      case .addContact(.presented(.saveButtonTapped)):
+        guard let contact = state.addContact?.contact
+        else { return .none }
+        state.contacts.append(contact)
+        state.addContact = nil
+        return .none
       case .addContact:
         return .none
       }
