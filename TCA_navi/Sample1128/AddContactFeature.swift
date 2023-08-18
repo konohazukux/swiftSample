00bf2dd9f2ad2500f737b404b1d505802c689b9e
takeshi shimada <konohazukux@gmail.com>
2023-08-18 15:49:57 +0900

    step4

--- TCA_navi/Sample1128/AddContactFeature.swift
+++ TCA_navi/Sample1128/AddContactFeature.swift
@@ -1,15 +1,6 @@
-//
-//  AddContactFeature.swift
-//  Sample1128
-//
-//  Created by TAKESHI SHIMADA on 2023/08/17.
-//  Copyright © 2023 TAKESHI SHIMADA. All rights reserved.
-//
-
 import ComposableArchitecture
-import SwiftUI
 
-struct AddContactFeature: ReducerProtocol {
+struct AddContactFeature: Reducer {
   struct State: Equatable {
     var contact: Contact
   }
@@ -18,59 +9,15 @@ struct AddContactFeature: ReducerProtocol {
     case saveButtonTapped
     case setName(String)
   }
-  func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
+  func reduce(into state: inout State, action: Action) -> Effect<Action> {
     switch action {
     case .cancelButtonTapped:
       return .none
-
     case .saveButtonTapped:
       return .none
-
     case let .setName(name):
       state.contact.name = name
       return .none
-    }
-  }
-}
-
-
-struct AddContactView: View {
-  let store: StoreOf<AddContactFeature>
-
-  var body: some View {
-    WithViewStore(self.store, observe: { $0 }) { viewStore in
-      Form {
-        TextField("Name", text: viewStore.binding(get: \.contact.name, send: { .setName($0) }))
-        Button("Save") {
-          viewStore.send(.saveButtonTapped)
-        }
-      }
-      .toolbar {
-        ToolbarItem {
-          Button("Cancel") {
-            viewStore.send(.cancelButtonTapped)
-          }
-        }
-      }
-    }
-  }
-}
-
-struct AddContactPreviews: PreviewProvider {
-  static var previews: some View {
-    NavigationStack {
-      AddContactView(
-        store: Store(
-          initialState: AddContactFeature.State(
-            contact: Contact(
-              id: UUID(),
-              name: "Blob"
-            )
-          )
-        ) {
-          AddContactFeature()
-        }
-      )
     }
   }
 }
