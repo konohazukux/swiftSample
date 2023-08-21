fec6a6827e5bce64fdcee3ef2653a77b9f6d1e94
takeshi shimada <konohazukux@gmail.com>
2023-08-21 16:37:40 +0900

    step 10

--- TCA_navi/Sample1128/ContactsFeature.swift
+++ TCA_navi/Sample1128/ContactsFeature.swift
@@ -24,8 +24,10 @@ struct ContactsFeature: Reducer {
     Reduce { state, action in
       switch action {
       case .addButtonTapped:
-        state.addContact = AddContactFeature.State(
-          contact: Contact(id: UUID(), name: "")
+        state.destination = .addContact(
+          AddContactFeature.State(
+            contact: Contact(id: UUID(), name: "")
+          )
         )
         return .none
 //      case .addContact(.presented(.delegate(.cancel))):
