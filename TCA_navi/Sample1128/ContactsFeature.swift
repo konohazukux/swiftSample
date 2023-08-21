257fceca385c5a6a91fee4b0e263290cce066cb2
takeshi shimada <konohazukux@gmail.com>
2023-08-21 09:23:46 +0900

    Step 8

--- TCA_navi/Sample1128/ContactsFeature.swift
+++ TCA_navi/Sample1128/ContactsFeature.swift
@@ -24,16 +24,16 @@ struct ContactsFeature: Reducer {
           contact: Contact(id: UUID(), name: "")
         )
         return .none
-      case .addContact(.presented(.delegate(.cancel))):
-        state.addContact = nil
-        return .none
+//      case .addContact(.presented(.delegate(.cancel))):
+//        state.addContact = nil
+//        return .none
       case let .addContact(.presented(.delegate(.saveContact(contact)))):
         state.contacts.append(contact)
-        state.addContact = nil
+//        state.addContact = nil
         return .none
-      case .addContact(.presented(.cancelButtonTapped)):
-        state.addContact = nil
-        return .none
+//      case .addContact(.presented(.cancelButtonTapped)):
+//        state.addContact = nil
+//        return .none
       case .addContact(.presented(.saveButtonTapped)):
         guard let contact = state.addContact?.contact
         else { return .none }
