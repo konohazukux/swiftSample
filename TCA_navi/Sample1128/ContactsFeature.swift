3174660e74a5380c7c0de0620ffd8ef0a518ccce
takeshi shimada <konohazukux@gmail.com>
2023-08-21 17:19:19 +0900

    step16 ~ step 19

--- TCA_navi/Sample1128/ContactsFeature.swift
+++ TCA_navi/Sample1128/ContactsFeature.swift
@@ -108,9 +108,8 @@ struct ContentView: View {
       }
       .sheet(
         store: self.store.scope(state: \.$destination, action: { .destination($0) }),
-        state: ,
-        action:
-        )
+        state: /ContactsFeature.Destination.State.addContact,
+        action: ContactsFeature.Destination.Action.addContact
       ) { addContactStore in
         NavigationStack {
           AddContactView(store: addContactStore)
@@ -118,8 +117,8 @@ struct ContentView: View {
       }
       .alert(
         store: self.store.scope(state: \.$destination, action: { .destination($0) }),
-        state: ,
-        action:
+        state: /ContactsFeature.Destination.State.alert,
+        action: ContactsFeature.Destination.Action.alert
       )
     }
   }
