386547cdf4f220eccafeaa8cd1729674130d2254
takeshi shimada <konohazukux@gmail.com>
2023-08-24 21:32:29 +0900

    todo first

--- TCA_TODO/Sample1128/TodosApp.swift
+++ TCA_TODO/Sample1128/TodosApp.swift
@@ -12,35 +12,3 @@ struct TodosApp: App {
   }
 }
 
-struct Todos: Reducer {
-  struct State {
-    @BindingState var editMode: EditMode = .inactive
-    var count = 0
-  }
-
-  enum Action {
-    case incrementButtonTapped
-  }
-
-  func reduce(into state: inout State, action: Action) -> Effect<Action> {
-    switch action {
-    case .incrementButtonTapped:
-      return .none
-    }
-  }
-}
-
-extension Todos.State: Equatable {}
-
-struct AppView: View {
-  let store: StoreOf<Todos>
-  var body: some View {
-    WithViewStore(self.store, observe: { $0 }) { viewStore in
-      Text("\(viewStore.count)")
-        .font(.largeTitle)
-        .padding()
-        .background(Color.black.opacity(0.1))
-        .cornerRadius(10)
-    }
-  }
-}
