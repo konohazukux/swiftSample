8d76f2e6a38102387fa223873b4c0abd12991c2f
takeshi shimada <konohazukux@gmail.com>
2023-09-05 17:25:00 +0900

    fix

--- Apollo_GraphQL_old/Sample1128/FirstViewController.swift
+++ Apollo_GraphQL_old/Sample1128/FirstViewController.swift
@@ -5,21 +5,30 @@
 
 import UIKit
 import Apollo
+import StarWarsAPI
 
 class FirstViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-    }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    getData()
+  }
   
   func getData() {
-    
+    let apollo = ApolloClient(url: URL(string: "https://swapi-graphql.netlify.app/.netlify/functions/index")!)
+    let query = Query()
+    apollo.fetch(query: query) { result in
+      switch result {
+      case .success(let graphQLResult):
+        print("Success! Result: \(graphQLResult)")
+      case .failure(let error):
+        print("Failure! Error: \(error)")
+      }
+    }
   }
-   
 }
+
 
