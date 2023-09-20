3a0477c05afca896cc9d77cec6979e18f22eefa9
takeshi shimada <konohazukux@gmail.com>
2023-09-20 10:56:22 +0900

    deeplink https://thwork.net/2021/08/29/swiftui_notification_deeplink/

--- SwiftUI_UI_local_deeplink/Sample1128/NotificationModel.swift
+++ SwiftUI_UI_local_deeplink/Sample1128/NotificationModel.swift
@@ -1,23 +1,22 @@
+import SwiftUI
 import UserNotifications
 
 class NotificationModel: ObservableObject {
-
-    let notificationIdentifier = "NotificationTest"
+    
     var notificationDelegate = ForegroundNotificationDelegate()
-
+    
     init() {
-        //フォアグラウンド通知用、バックグラウンドのみなら不要
         UNUserNotificationCenter.current().delegate = self.notificationDelegate
     }
 
-    func setNotification(){
+    func setNotification(genre:String ,item:String){
 
         UNUserNotificationCenter.current().requestAuthorization(
             options: [.alert, .sound, .badge]){
             (granted, _) in
             if granted {
                 //許可
-                self.makeNotification()
+                self.makeNotification(genre:genre ,item:item)
             }else{
                 //非許可
             }
@@ -25,7 +24,7 @@ class NotificationModel: ObservableObject {
 
     }
 
-    func makeNotification(){
+    func makeNotification(genre:String ,item:String){
 
         //日時
         let notificationDate = Date().addingTimeInterval(10)//10秒後
@@ -36,29 +35,36 @@ class NotificationModel: ObservableObject {
 
         //通知内容
         let content = UNMutableNotificationContent()
-        content.title = "NotificationTitle"
-        content.body = "NotificationBody"
+        content.title = "DeepLinkTest"
+        content.body = "\(genre):\(item)"
         content.sound = UNNotificationSound.default
 
         //リクエスト作成
-        let request = UNNotificationRequest(identifier: self.notificationIdentifier, content: content, trigger: trigger)
+        let identifier = "Genre=\(genre)&Item=\(item)"
+        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
 
         //通知をセット
         UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
     }
-
-    func removeNotification(){
-        UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [self.notificationIdentifier])
-    }
 }
 
 
-//フォアグラウンド通知用、バックグラウンドのみなら不要
-class ForegroundNotificationDelegate:NSObject,UNUserNotificationCenterDelegate{
+class ForegroundNotificationDelegate:NSObject, UNUserNotificationCenterDelegate{
     
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         
         //completionHandler([.alert, .list, .badge, .sound]) //~iOS13
         completionHandler([.banner, .list, .badge, .sound]) //iOS14~
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        
+        let queryString = response.notification.request.identifier
+        let url = URL(string:"deeplinktest://deeplink?\(queryString)")
+        if let openUrl = url {
+            UIApplication.shared.open(openUrl)
+        }
+        
+        completionHandler()
     }
 }
