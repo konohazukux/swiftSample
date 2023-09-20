23bd43270c9dab74f58565a5af2e8aa126d2bc0e
takeshi shimada <konohazukux@gmail.com>
2023-09-20 11:27:22 +0900

    fix

---
+++ SwiftUI_UI_local_ObservableObject/Sample1128/App.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+import Combine
+
+@main
+struct DeepLinkTestTestApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
+
+struct ContentView: View {
+  var body: some View {
+    Text("")
+  }
+}
+
+
