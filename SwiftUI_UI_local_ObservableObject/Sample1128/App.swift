6559ae88cfae714bb09ab0d4c2202dd39eed7b06
takeshi shimada <konohazukux@gmail.com>
2023-09-21 09:45:11 +0900

    observable object

--- SwiftUI_UI_local_ObservableObject/Sample1128/App.swift
+++ SwiftUI_UI_local_ObservableObject/Sample1128/App.swift
@@ -5,15 +5,46 @@ import Combine
 struct DeepLinkTestTestApp: App {
   var body: some Scene {
     WindowGroup {
-      ContentView()
+      ContentView().environmentObject(EnvironmentModel())
     }
   }
 }
 
 struct ContentView: View {
+ 
+//  @ObservedObject var obsModel = ObserveModel()
+  var obsModel = ObserveModel()
+  @EnvironmentObject var envModel: EnvironmentModel
+  
   var body: some View {
-    Text("")
+    VStack {
+      Text("obsCount: \(self.obsModel.count)")
+        .padding()
+      Button(action: {
+        self.obsModel.count += 1
+      }, label: {
+        Text("ObsCount up")
+      })
+     
+      Text("EnvCount: \(self.envModel.count)")
+        .padding()
+      Button(action: {
+        self.envModel.count += 1
+      }, label: {
+        Text("EnvCount up")
+      })
+      
+    }
   }
 }
 
+class ObserveModel: ObservableObject {
+  @Published var count = 0
+//  var count = 0
+}
+
+class EnvironmentModel: ObservableObject {
+  @Published var count = 0
+//  var count = 0
+}
 
