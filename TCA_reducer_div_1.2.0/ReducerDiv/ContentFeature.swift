751a8cc6900e7fd9b6631f76073c1e6f790fda2b
takeshi shimada <konohazukux@gmail.com>
2023-10-26 17:30:30 +0900

    fix

--- TCA_reducer_div_1.2.0/ReducerDiv/ContentFeature.swift
+++ TCA_reducer_div_1.2.0/ReducerDiv/ContentFeature.swift
@@ -8,9 +8,14 @@ import ComposableArchitecture
 
 
 struct ContentFeature: Reducer {
-  struct State: Equatable { }
-  enum Action: Equatable { }
+  struct State: Equatable {
+    var tap = TapFeature.State()
+  }
+  enum Action: Equatable {
+    case tap(TapFeature.Action)
+  }
   var body: some ReducerOf<Self> {
+    Scope(state: \.tap, action: /Action.tap, child: { TapFeature() })
     Reduce { state, action in
       switch action {
       default:
