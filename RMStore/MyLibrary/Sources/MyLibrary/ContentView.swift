be62690287e1f20490003456ad3f997627cb9f97
takeshi shimada <konohazukux@gmail.com>
2023-10-30 13:08:48 +0900

    fix

--- RMStore/MyLibrary/Sources/MyLibrary/ContentView.swift
+++ RMStore/MyLibrary/Sources/MyLibrary/ContentView.swift
@@ -1,14 +1,17 @@
 //
-//  ContentView.swift
-//  Sample
+//  File.swift
+//  
 //
 //  Created by TAKESHI SHIMADA on 2023/10/30.
 //
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
+public struct ContentView: View {
+  
+  public init() {}
+
+    public var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
--- RMStore/Sample/Sample/SampleApp.swift
+++ RMStore/Sample/Sample/SampleApp.swift
@@ -6,12 +6,13 @@
 //
 
 import SwiftUI
+import MyLibrary
 
 @main
 struct SampleApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+          ContentView()
         }
     }
 }
