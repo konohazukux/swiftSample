a5eb19e3994f72f99ad5a4f50f72964d912f4856
takeshi shimada <konohazukux@gmail.com>
2023-11-16 16:48:28 +0900

    reducer 切り出し

--- TCA_reducer_div/Sample1128/CounterView.swift
+++ TCA_reducer_div/Sample1128/CounterView.swift
@@ -12,14 +12,14 @@ struct CounterView: View {
   var body: some View {
     WithViewStore(self.store, observe: { $0 }) { viewStore in
       VStack {
-        Text("\(viewStore.count)")
+        Text("\(viewStore.hoge.count)")
           .font(.largeTitle)
           .padding()
           .background(Color.black.opacity(0.1))
           .cornerRadius(10)
         HStack {
           Button("+"){
-            viewStore.send(.incrementButtonTapped)
+            viewStore.send(.hoge(.incrementButtonTapped))
           }
             .font(.largeTitle)
             .padding()
