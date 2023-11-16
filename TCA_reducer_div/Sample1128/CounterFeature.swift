5dd33832f7f696450d24e66a025baa0a35d6c600
takeshi shimada <konohazukux@gmail.com>
2023-11-16 17:15:29 +0900

    reducer切り出し

--- TCA_reducer_div/Sample1128/CounterFeature.swift
+++ TCA_reducer_div/Sample1128/CounterFeature.swift
@@ -7,7 +7,38 @@ import SwiftUI
 import ComposableArchitecture
 
 struct CounterFeature: Reducer {
+ 
+  struct State: Equatable {
+    var hoge = IncrementFeature.State()
+  }
   
+  @CasePathable
+  enum Action: Equatable {
+    case hoge(IncrementFeature.Action)
+  }
+  
+  enum CancelID { case timer }
+ 
+  var body: some ReducerOf<Self> {
+    
+    Scope(state: \.hoge, action: \.hoge) {
+      IncrementFeature()
+    }
+    
+    Reduce { state, action in
+      switch action {
+      case .hoge(let action):
+        switch(action) {
+        case .incrementButtonTapped:
+          print("#tag115 \(Date().ISO8601Format()) \(#line) \(type(of: self))  \(#function) : \(self) ")
+        }
+        return .none
+      }
+    }
+  }
+}
+
+struct IncrementFeature: Reducer {
   struct State: Equatable {
     var count = 0
   }
@@ -15,15 +46,15 @@ struct CounterFeature: Reducer {
   enum Action: Equatable {
     case incrementButtonTapped
   }
-  
   var body: some ReducerOf<Self> {
     Reduce { state, action in
       switch action {
       case .incrementButtonTapped:
+        print("#tag112 \(Date().ISO8601Format()) \(#line) \(type(of: self))  \(#function) : \(self) ")
         state.count += 1
         return .none
       }
     }
   }
 }
-  
+
