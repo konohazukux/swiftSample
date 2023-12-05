1f31fd9876c6174f2708391893600dae234c57af
takeshi shimada <konohazukux@gmail.com>
2023-12-05 09:16:25 +0900

    first commit

---
+++ 231205_TCA_Test/TCA_Test_1205/TCA_Test_1205UITests/TCA_Test_1205UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TCA_Test_1205UITestsLaunchTests.swift
+//  TCA_Test_1205UITests
+//
+//  Created by takeshishimada on 2023/12/05.
+//
+
+import XCTest
+
+final class TCA_Test_1205UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
