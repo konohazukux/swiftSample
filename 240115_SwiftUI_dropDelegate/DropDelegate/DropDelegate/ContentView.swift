08a130b891f2062d2a43ef36fbcd3fdfaf43a8c1
takeshi shimada <konohazukux@gmail.com>
2024-01-16 10:42:44 +0900

    drag and drop

--- 240115_SwiftUI_dropDelegate/DropDelegate/DropDelegate/ContentView.swift
+++ 240115_SwiftUI_dropDelegate/DropDelegate/DropDelegate/ContentView.swift
@@ -2,15 +2,15 @@
 //  ContentView.swift
 //  DropDelegate
 //
-//  Created by TAKESHI SHIMADA on 2024/01/15.
-//
 
 import SwiftUI
+import UniformTypeIdentifiers
 
 struct ContentView: View {
   
-  let items = 1...1000
-  let columns = [
+  @State var items = Array(1...1000)
+  @State var draggedItem: Int?
+  var columns = [
     GridItem(.adaptive(minimum: 100))
   ]
   
@@ -19,9 +19,46 @@ struct ContentView: View {
         LazyVGrid(columns: columns, spacing: 20) {
           ForEach(items, id: \.self) { item in
             Text("Item \(item)")
+              .padding()
+               .frame(height: 100)
+               .background(Color.blue)
+               .cornerRadius(10)
+               .onDrag {
+                 self.draggedItem = item
+                 return NSItemProvider(object: String(item) as NSString)
+               }
+               .onDrop(
+                of: [String(UTType.plainText.identifier)],
+                delegate: DropViewDelegate(
+                  item: item,
+                  items: $items,
+                  draggedItem: $draggedItem
+                )
+               )
           }
         }
       }
+    }
+}
+
+struct DropViewDelegate: DropDelegate {
+    let item: Int
+    @Binding var items: [Int]
+    @Binding var draggedItem: Int?
+    
+    func performDrop(info: DropInfo) -> Bool {
+        guard let draggedItem = draggedItem else { return false }
+        
+        if item != draggedItem {
+            let from = items.firstIndex(of: draggedItem)!
+            let to = items.firstIndex(of: item)!
+            if from != to {
+                withAnimation {
+                    items.move(fromOffsets: IndexSet(integer: from), toOffset: to > from ? to + 1 : to)
+                }
+            }
+        }
+        return true
     }
 }
 
